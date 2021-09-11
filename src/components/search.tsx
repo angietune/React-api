6aa680a622d22127b646a2711a775d0581911ee0
angietune <75065099+angietune@users.noreply.github.com>
2021-09-11 17:27:47 +0300

    feat: add styles

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import '../styles.scss'
-import Result from "./result";
+import React, { useState } from 'react';
+import '../styles.scss';
+import Result from './result';
 
 const Searchbar = () => {
     const [query, setQuery] = useState('');
@@ -9,8 +9,8 @@ const Searchbar = () => {
     const [loading, setLoading] = useState(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setQuery(e.target.value)
-    }
+        setQuery(e.target.value);
+    };
 
     const accesskey = '54aaba10eb5f2aa6e77f7549526e8b8d';
 
@@ -24,43 +24,48 @@ const Searchbar = () => {
         extras: 'url_m',
         format: 'json',
         nojsoncallback: '1',
-    }
+    };
 
-    const parameters = new URLSearchParams(flickrData)
+    const parameters = new URLSearchParams(flickrData);
 
-    const flickrUrl = `https://api.flickr.com/services/rest/?${parameters}`
+    const flickrUrl = `https://api.flickr.com/services/rest/?${parameters}`;
 
-    const searchPhotos = (e: React.MouseEvent<HTMLElement>) => {
-        e.preventDefault()
-        setLoading(true)
+    const searchPhotos = async (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        setLoading(true);
         fetch(flickrUrl)
-        .then(response => {return response.json()})
-        .then((data) => {
-            setPics(data.photos.photo)
-            setPages(data.photos.pages)
-        })
-        setLoading(false)
-    }
-
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setPics(data.photos.photo);
+                setPages(data.photos.pages);
+            });
+        setLoading(false);
+    };
 
     return (
         <div>
             <form className="search-form">
-            <input className="input" type="text"
-                onChange={handleChange}
-                value={query}
-                placeholder="Enter some keywords"
-            />
-            <button className="button" onClick={searchPhotos}>Search</button>
+                <input
+                    className="input"
+                    type="text"
+                    onChange={handleChange}
+                    value={query}
+                    placeholder="Enter some keywords"
+                />
+                <button className="button" onClick={searchPhotos}>
+                    Search
+                </button>
             </form>
             <div className="info-list">
                 <div>
                     <p>Total results: {pages}</p>
                 </div>
             </div>
-            <Result pics={pics} loading={loading}/>
+            <Result pics={pics} loading={loading} />
         </div>
-    )
-}
+    );
+};
 
-export default Searchbar
+export default Searchbar;
