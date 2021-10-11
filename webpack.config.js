400b16592245df43c40707d565073e875c30ff72
Your Name <you@example.com>
2021-10-11 11:35:43 +0300

    feat: add config params for routing

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,34 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const devServer = (isDev) => !isDev ? {} : {
-    devServer: {
-      open: true,
-      port: 8080,
-      contentBase: path.join(__dirname, 'public'),
-    },
-  };
+const devServer = (isDev) =>
+    !isDev
+        ? {}
+        : {
+              devServer: {
+                  open: true,
+                  port: 8080,
+                  contentBase: path.join(__dirname, 'public'),
+                  historyApiFallback: true,
+              },
+          };
 
 module.exports = ({ development }) => ({
     mode: development ? 'development' : 'production',
     devtool: development ? 'inline-source-map' : false,
     entry: {
-        app: path.join(__dirname, 'src', 'index.tsx')
+        app: path.join(__dirname, 'src', 'index.tsx'),
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: ['.ts', '.tsx', '.js'],
+        modules: ['src', 'node_modules'],
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
-                exclude: '/node_modules/'
+                exclude: '/node_modules/',
             },
             {
                 test: /\.(?:ico|gif|png|jpg|jpeg|svg|webp)$/i,
@@ -42,25 +47,23 @@ module.exports = ({ development }) => ({
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            }
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
         ],
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
         new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'index.html')
+            template: path.join(__dirname, 'src', 'index.html'),
         }),
         new CopyPlugin({
-            patterns: [
-              { from: 'public' },
-            ],
+            patterns: [{ from: 'public' }],
         }),
         new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     ],
-    ...devServer(development)
+    ...devServer(development),
 });
